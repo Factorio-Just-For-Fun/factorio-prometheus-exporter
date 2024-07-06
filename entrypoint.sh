c69cc1e3cb60bfa84b2e09afded5c39b1d27353b
joloman2 <eshuis.nathan@gmail.com>
2024-07-06 20:21:21 +0200

    bug fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,24 @@ check_config() {
   fi
 }
 
+# Function to perform the rename operation
+rename_config() {
+  echo "Renaming config.yaml.example to config.yaml..."
+  mv /app/config.yaml.example /app/config.yaml
+}
+
 # Wait for 5 seconds to allow the container to fully start up
 sleep 5
 
 # Check if config.yaml already exists
 if ! check_config; then
-  echo "Config file config.yaml not found. Renaming config.yaml.example..."
-  mv /app/config.yaml.example /app/config.yaml
+  echo "Config file config.yaml not found. Attempting to rename config.yaml.example..."
+
+  # Attempt to rename up to 3 times with increasing delay (1, 2, 3 seconds)
+  for i in {1..3}; do
+    rename_config && break
+    sleep $i
+  done
 
   # Check if rename was successful
   if ! check_config; then
